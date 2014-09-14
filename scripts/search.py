d5c100f1e9991ad54bf2e148c3f49ef0a8336054
James Michael DuPont <JamesMikeDuPont@gmail.com>
2014-09-14 08:09:15 -0500

    adding a search first

--- scripts/search.py
+++ scripts/search.py
@@ -6,19 +6,29 @@ n= netrc.netrc()
 
 g = Github(login, password)
 
-users = g.search_users("", location="Kansas")
-
-for person in users:
-#    print x
-    status =g.get_user().add_to_following(person)
-    # if not person.has_in_following():
-    print "Adding",status, person.bio, person.email, person.location
-        
-
+users = g.search_users("", location="OK")
+seen ={}
 
 def all_following():
     for person in g.get_user().get_following():
-        print person.bio
-        print person.email
-        print person.location
-        print person.gravatar_id
+        seen[person.url] =1
+        #print person.url
+        # print person.bio
+        # print person.email
+        # print person.location
+        # print person.gravatar_id
+
+print "loading existing users"
+all_following()
+
+print "scanning new users"
+for person in users:
+#    print x
+    if person.url not in seen :
+        print person.url,person.bio, person.email, person.location
+
+        status =g.get_user().add_to_following(person)
+        # if not person.has_in_following():
+        print "Adding",status, person.bio, person.email, person.location
+        
+

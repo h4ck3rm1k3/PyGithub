00432933fbdc750f17dc60b97cdd49db3ff1f2d1
James Michael DuPont <JamesMikeDuPont@gmail.com>
2014-09-15 22:07:23 -0500

    adding in a sleep , but my account seems to be over limit

--- scripts/search.py
+++ scripts/search.py
@@ -1,12 +1,14 @@
 from github import Github
-
+import time
 import netrc
+
+followed = 0 
 n= netrc.netrc()
 (login,account,password) = n.authenticators("github")
 
 g = Github(login, password)
 
-users = g.search_users("", location="OK")
+
 seen ={}
 
 def all_following():
@@ -20,15 +22,20 @@ def all_following():
 
 print "loading existing users"
 all_following()
-
 print "scanning new users"
-for person in users:
-#    print x
-    if person.url not in seen :
-        print person.url,person.bio, person.email, person.location
 
-        status =g.get_user().add_to_following(person)
-        # if not person.has_in_following():
-        print "Adding",status, person.bio, person.email, person.location
-        
-
+for state in ( "MI", "MN", "WI", "ND", "IL", "MN", "KS", "OK", "NE", "CO", "TX", "WY", "SD", "IA", "AR", "KS", "MO"):
+    print "searching for state %s" % state
+    users = g.search_users("", location=state)
+    for person in users:
+        if person.url not in seen :
+            print person.url,person.bio, person.email, person.location
+            status =g.get_user().add_to_following(person)
+            # if not person.has_in_following():
+            print "Adding",status, person.bio, person.email, person.location
+            followed = followed +1
+            if followed > 4999:
+                print "going to sleep for one hour to avoid overusing the api"
+                #sleep for one hour, you have 5000 api calls per hour https://developer.github.com/v3/#rate-limiting
+                time.sleep(3601)
+                followed = 0 # reset the count
